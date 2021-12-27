3f5bcecc371eed2bea310fad99bcc4085e6754e7
Rivani Arvian Pratama <arviannewbie473@gmail.com>
2021-12-28 02:58:16 +0700

    update (research penyedia API jadwal sholat lain)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import localeId from '@angular/common/locales/id';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeId, 'id');
+import { DatePipe } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ModalJadwalComponent } from './jadwal-sholat/modal-jadwal/modal-jadwal.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, ModalJadwalComponent],
-  entryComponents: [ModalJadwalComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [
+    AppComponent, 
+    ModalJadwalComponent
+  ],
+  entryComponents: [
+    ModalJadwalComponent
+  ],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    DatePipe,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: LOCALE_ID, useValue: "id-ID" }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,134 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { CommonService } from './common.service';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  serverUrl='https://api.sunhouse.co.id/lms/index.php/';
-  public photoBaseUrl='https://api.sunhouse.co.id/lms/photos/';
-  httpOption:any;
+  
+  city:any = 'yogyakarta';
   constructor(
-    public http:HttpClient
+    public http:HttpClient,
+    private common: CommonService,
+    private datePipe: DatePipe
   ) 
   {
     
   }
-  token:any;
-  getToken()
-  {
-    var tokens=localStorage.getItem('lmsToken');
-    this.token={email:'',jwt:''};
-    if(tokens!=null)
-    {
-      this.token=JSON.parse(tokens);      
-    }
-    this.httpOption = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Bearer '+this.token.jwt
-      })
-    };
-  }
-  generateOption(bearer)
-  {
-    this.httpOption = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Bearer '+bearer
-      })
-    };
-  }
-  me()
-  {
-    this.getToken();
-    return this.http.get(this.serverUrl+'users/me', this.httpOption);
-  }  
-  get(url)
-  {
-    this.getToken();
-    return this.http.get(this.serverUrl+url, this.httpOption);
-  }
-  getWithBearer(url,bearer)
-  {
-    this.generateOption(bearer);
-    return this.http.get(this.serverUrl+url, this.httpOption);
-  }
-  post(url,data)
-  {
-    this.getToken();
-    return this.http.post(this.serverUrl+url,data,  this.httpOption);
-  }
-  postWithBearer(url,data,bearer)
-  {
-    this.generateOption(bearer);
-    return this.http.post(this.serverUrl+url,data, this.httpOption);
-  }
-  put(url,data)
-  {
-    this.getToken();
-    return this.http.put(this.serverUrl+url,data,  this.httpOption);
-  }
-  putWithBearer(url,data,bearer)
-  {
-    this.generateOption(bearer);
-    return this.http.put(this.serverUrl+url,data, this.httpOption);
-  }
-  delete(url)
-  {
-    this.getToken();
-    return this.http.delete(this.serverUrl+url,  this.httpOption);
-  }
-  import(file)
-  {
-    //this.getToken();
-    return this.http.post(this.serverUrl+'import',file);
+
+  async getToday() {
+    let date = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    let data = await this.common.get('day.json?city='+ this.city +'&date='+ date +'&school=1');
+    return data;
   }
 
-  upload(url,data)
-  {
-    this.getToken();
-    this.httpOption = {
-      headers: new HttpHeaders({       
-        'Authorization': 'Bearer '+this.token.jwt
-      })
-    };
-    return this.http.post(this.serverUrl+url,data,  this.httpOption);
+  async getWeek() {
+    let data = await this.common.get('this_week.json?city='+ this.city +'&school=1');
+    return data;
   }
 
-  uploadImage(url,data) {
-    this.getToken();
-    this.httpOption = {
-      headers: new HttpHeaders({       
-        'Authorization': 'Bearer '+this.token.jwt
-      })
-    };
-
-    return this.http.post(this.serverUrl+url,data,  this.httpOption);
+  async getMonth() {
+    let data = await this.common.get('this_month.json?city='+ this.city +'&school=1');
+    return data;
   }
-
-  uploadDoc(url,data) {
-    this.getToken();
-    this.httpOption = {
-      headers: new HttpHeaders({       
-        'Authorization': 'Bearer '+this.token.jwt
-      })
-    };
-
-    return this.http.post(this.serverUrl+url,data,  this.httpOption);
-  }
-
-  user()
-  {
-    var tokens=localStorage.getItem('lmsToken');
-    this.token={email:'',jwt:'',username:'',tahunPelajaran:'',semester:''};
-    if(tokens!=null)
-    {
-      this.token=JSON.parse(tokens);      
-    }  
-    return this.token; 
-  }
-
 
 }
