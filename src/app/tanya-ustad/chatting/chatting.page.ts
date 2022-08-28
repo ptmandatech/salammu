c73927dbb5707a32c3308ded543f3868cd1a65d3
whois arvian <35618379+whois-arvian@users.noreply.github.com>
2022-08-29 03:49:19 +0700

    update chat

--- src/app/tanya-ustad/chatting/chatting.page.ts
+++ src/app/tanya-ustad/chatting/chatting.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActionSheetController, IonContent } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { ActionSheetController, IonContent, LoadingController, ToastController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-chatting',
@@ -8,49 +10,125 @@ import { ActionSheetController, IonContent } from '@ionic/angular';
 })
 export class ChattingPage implements OnInit {
 
-  messages = [
-    {
-
-      createdAt: 1554090856000,
-      msg: 'Saya ada masalah pada tanaman saya yang kurang subur'
-    },
-    {
-      createdAt: 1554090956000,
-      msg: 'Tanaman apa nggih pak ?'
-    },
-    {
-      user: 'Fathu Rizqillah',
-      createdAt: 1554091056000,
-      msg: 'Tanaman Padi pak sekitar umur 90 HST'
-    }
-  ];
-
-  currentUser = 'Fathu Rizqillah';
   newMsg = '';
   @ViewChild(IonContent, {static: false}) content: IonContent;
-
+  data:any = {};
+  roomData:any;
+  loading:boolean;
+  listChats:any = [];
+  userData:any;
 
   constructor(
+    public api: ApiService,
+    private route: ActivatedRoute,
+    private toastController: ToastController,
+    private loadingController: LoadingController,
     public actionSheetController: ActionSheetController
-  ) { }
+  ) { 
+    this.route.queryParams.subscribe((data: any) => {
+      if (data && data.data) {
+        this.data = JSON.parse(data.data);
+        this.checkRoom();
+      }
+    });
+  }
 
   ngOnInit() {
+    this.present();
+    this.cekLogin();
+  }
+
+  async present() {
+    this.loading = true;
+    return await this.loadingController.create({
+      spinner: 'crescent',
+      duration: 10000,
+      message: 'Tunggu Sebentar...',
+      cssClass: 'custom-class custom-loading'
+    }).then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.loading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+          this.loading = false;
+        }
+      });
+      this.loading = false;
+    });
+  }
+
+  cekLogin()
+  {    
+    this.api.me().then(res=>{
+      this.userData = res;
+      this.loadingController.dismiss();
+    }, error => {
+      this.loadingController.dismiss();
+    })
+  }
+
+  checkRoom() {
+    this.api.post('chattings/checkRooms', this.data).then(res => {
+      this.checkResult(res);
+    })
+  }
+
+  roomExist:boolean;
+  checkResult(res) {
+    if(res == 'not_exist') {
+      this.roomExist = false;
+    } else {
+      this.roomExist = true;
+      this.roomData = res;
+      this.getChats();
+    }
+
+    if(this.newMsg != '') {
+      this.sendMessage();
+    }
+  }
+
+  createRooms() {
+    this.data.id = new Date().getTime().toString() + '' + [Math.floor((Math.random() * 1000))],
+    this.api.post('chattings/rooms', this.data).then(res => {
+      this.checkRoom();
+    })
+  }
+
+  getChats() {
+    this.api.get('chattings/getChats/'+this.roomData.id).then(res => {
+      this.listChats = res;
+  
+      setTimeout(() => {
+        this.content.scrollToBottom(400);
+      }, 1000);
+    })
   }
 
   sendMessage() {
-    this.messages.push({
-      user: 'Fathu Rizqillah',
-      createdAt: new Date().getTime(),
-      msg: this.newMsg
-    });
-
-    this.newMsg = '';
-
-    setTimeout(() => {
-      this.content.scrollToBottom(400);
-    });
+    if(this.roomExist) {
+      let dt = {
+        id: new Date().getTime().toString() + '' + [Math.floor((Math.random() * 1000))],
+        created_by: this.userData.id,
+        room_id: this.roomData.id,
+        messages: this.newMsg,
+        ustad_already_read: this.roomData.ustadz_id == this.userData.id ? true:false,
+        user_already_read: this.roomData.ustadz_id != this.userData.id ? true:false,
+        type: this.image == null ? 'text':'image'
+      }
+  
+      this.newMsg = '';
+  
+      this.api.post('chattings/chats', dt).then(res => {
+        console.log(res)
+        this.getChats();
+      });
+    } else {
+      this.createRooms();
+    }
   }
 
+  image:any;
   async lampiran() {
     const actionSheet = await this.actionSheetController.create({
       header: 'Pilih',
--- src/app/tanya-ustad/tanya-ustad.page.ts
+++ src/app/tanya-ustad/tanya-ustad.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { LoginPage } from '../auth/login/login.page';
+import { ApiService } from '../services/api.service';
+import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-tanya-ustad',
@@ -8,10 +13,208 @@ import { Component, OnInit } from '@angular/core';
 export class TanyaUstadPage implements OnInit {
 
   defaultSegment:any='list';
-  
-  constructor() { }
+  listUstadz:any = [];
+  listRoomChats:any = [];
+  listRoomChatsTemp:any = [];
+  listUstadzTemp:any = [];
+  serverImg: any;
+  loading:boolean;
+  userData:any;
+  unreadTotal = 0;
+  constructor(
+    public api: ApiService,
+    public common: CommonService,
+    public router:Router,
+    public modalController: ModalController,
+    private toastController: ToastController,
+    private loadingController: LoadingController,
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.present();
+    this.serverImg = this.common.photoBaseUrl+'pediamu/';
+    this.listUstadz = [];
+    this.listUstadzTemp = [];
+    this.getAllUstadz();
+    this.cekLogin();
+  }
+
+  ionViewWillEnter() {
+    this.ngOnInit();
+  }
+
+  async doRefresh(event) {
+    this.loading = true;
+    this.listUstadz = [];
+    this.listUstadzTemp = [];
+    this.present();
+    this.getAllUstadz();
+    this.cekLogin();
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
+
+  async present() {
+    this.loading = true;
+    return await this.loadingController.create({
+      spinner: 'crescent',
+      duration: 10000,
+      message: 'Tunggu Sebentar...',
+      cssClass: 'custom-class custom-loading'
+    }).then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.loading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+          this.loading = false;
+        }
+      });
+      this.loading = false;
+    });
+  }
+
+  cekLogin()
+  {    
+    this.api.me().then(res=>{
+      this.userData = res;
+      this.getAvailableRoomChats();
+      this.loadingController.dismiss();
+    }, error => {
+      console.log(error)
+      this.loadingController.dismiss();
+    })
+  }
+
+  getAllUstadz() {
+    this.listUstadz = [];
+    this.listUstadzTemp = [];
+    this.api.get('ustadzmu').then(res => {
+      this.listUstadz = res;
+      this.listUstadzTemp = res;
+    }, error => {
+      this.loading = false;
+    })
+  }
+
+  newMessage(n, status) {
+    if(this.userData) {
+      let dt;
+      if(status == 'new') {
+        dt = {
+          ustadz_id: n.id,
+          ustadz_name: n.name,
+          user_id: this.userData.id,
+        }
+      } else {
+        dt = {
+          ustadz_id: n.ustadz_id,
+          ustadz_name: n.ustadz_name,
+          user_id: this.userData.id,
+        }
+      }
+      const param: NavigationExtras = {
+        queryParams: {
+          data: JSON.stringify(dt)
+        }
+      }
+      this.router.navigate(['/tanya-ustad/chatting'], param);
+    } else {
+      this.toastController
+      .create({
+        message: 'Anda harus login terlebih dahulu.',
+        duration: 2000,
+        color: "danger",
+      })
+      .then((toastEl) => {
+        toastEl.present();
+      });
+      this.modalLogin();
+    }
+  }
+
+  getAvailableRoomChats() {
+    this.unreadTotal = 0;
+    this.listRoomChats = [];
+    this.listRoomChatsTemp = [];
+    this.api.get('chattings/getRooms/'+this.userData.id).then(res => {
+      this.listRoomChats = res;
+      this.listRoomChatsTemp = res;
+      this.listRoomChats.forEach(data => {
+        if(data.user_already_read == '0') {
+          this.unreadTotal += 1;
+        }
+      });
+    })
+  }
+
+  async modalLogin() {
+    await this.cekLogin();
+    let userData = JSON.parse(localStorage.getItem('salammuToken'));
+    if(userData == undefined) {
+      const modal = await this.modalController.create({
+        component: LoginPage,
+        mode: "md",
+      });
+      modal.onDidDismiss().then(async res => {
+        this.userData = undefined;
+        this.cekLogin();
+      })
+      return await modal.present();
+    }
+  }
+
+  initializeItems(): void {
+    if(this.defaultSegment == 'list') {
+      this.listUstadz = this.listUstadzTemp;
+    } else {
+      this.listRoomChats = this.listRoomChatsTemp;
+    }
+  }
+
+  searchTerm: string = '';
+  searchChanged(evt) {
+
+    this.initializeItems();
+
+    const searchTerm = evt.srcElement.value;
+
+    if (!searchTerm) {
+      return;
+    }
+
+    if(this.defaultSegment == 'list') {
+      this.listUstadz = this.listUstadz.filter(ustadz => {
+        if (ustadz.name && searchTerm) {
+          if (ustadz.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+            return true;
+          }
+          if (ustadz.science_name && searchTerm) {
+            if (ustadz.science_name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+              return true;
+            }
+            return false;
+          }
+          return false;
+        }
+      });
+    } else {
+      this.listRoomChats = this.listRoomChats.filter(rooms => {
+        if (rooms.ustadz_name && searchTerm) {
+          if (rooms.ustadz_name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+            return true;
+          }
+          if (rooms.lastMessages && searchTerm) {
+            if (rooms.lastMessages.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+              return true;
+            }
+            return false;
+          }
+          return false;
+        }
+      });
+    }
   }
 
 }
